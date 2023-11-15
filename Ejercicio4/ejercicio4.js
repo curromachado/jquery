ca3804ef828315d2f3c4d0c4045236266716a8c7
curromachado <franciscocarmona01@iesamachado.org>
2023-11-15 17:49:58 +0100

    Ejercicio4 realizado

---
+++ Ejercicio4/ejercicio4.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $("#btn-aumentar").click(function () { 
+        let actual = $(".pares").css("font-size"); //Cogemos el tamaño de fuente actual
+
+        let tamanoActual = parseInt(actual); //Convertimos el tamaño actual a un numero entero
+
+        let tamanoNuevo = tamanoActual + 2; //Aplicamos que suba de dos en dos px
+
+        $(".pares").css({ "font-size": tamanoNuevo + "px",
+         "background-color" : "red" });
+        
+       
+    });
+
+    $("#btn-disminuir").click(function () { 
+        let actual = $(".pares").css("font-size"); //Cogemos el tamaño de fuente actual
+
+        let tamanoActual = parseInt(actual); //Convertimos el tamaño actual a un numero entero
+
+        let tamanoNuevo = tamanoActual - 2; //Aplicamos que suba de dos en dos px
+
+        $(".pares").css({ "font-size": tamanoNuevo + "px",
+        "background-color" : "yellow" });
+    });
+});
