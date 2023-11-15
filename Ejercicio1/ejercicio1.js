9461cb310897e8e847ef3c2444bf529b95b4772f
curromachado <franciscocarmona01@iesamachado.org>
2023-11-15 17:01:43 +0100

    ejercicio1.1

--- Ejercicio1/ejercicio1.js
+++ Ejercicio1/ejercicio1.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     $("#btn-ocultar").click(function () { 
         $("p").hide();
-        
+        $("h1").hide();
     });
 
     $("#btn-mostrar").click(function () { 
         $("p").show();
-        
+        $("h1").show();
     });
 });
